e93ae0caa1d7beb047487ece9e3ee5d61fc257b2
from1998 <1967236098@qq.com>
2021-07-15 14:41:48 +0800

    首页及初步配置

---
+++ src/views/login/index.vue
@@ -0,0 +1,424 @@
+<template>
+  <div class="login-container">
+    <div class="logo">
+      <img src="../../assets/images/logo.png" alt="一流云停后台管理系统">
+    </div>
+    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
+
+      <div class="title-container">
+        <h3 class="title">一流云停后台管理系统</h3>
+      </div>
+
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <svg-icon icon-class="user" />
+        </span>
+        <el-input
+          ref="username"
+          v-model="loginForm.username"
+          placeholder="用户名"
+          name="username"
+          type="text"
+          tabindex="1"
+          autocomplete="on"
+        />
+      </el-form-item>
+
+      <el-tooltip v-model="capsTooltip" content="已开启大写键盘" placement="right" manual>
+        <el-form-item prop="password">
+          <span class="svg-container">
+            <svg-icon icon-class="password" />
+          </span>
+          <el-input
+            :key="passwordType"
+            ref="password"
+            v-model="loginForm.password"
+            :type="passwordType"
+            placeholder="密码"
+            name="password"
+            tabindex="2"
+            autocomplete="on"
+            @keyup.native="checkCapslock"
+            @blur="capsTooltip = false"
+          />
+          <span class="show-pwd" @click="showPwd">
+            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+          </span>
+        </el-form-item>
+      </el-tooltip>
+
+      <el-form-item prop="verifyCode">
+        <el-row>
+          <el-col :span="16">
+            <span class="svg-container">
+              <i class="el-icon-info" />
+            </span>
+            <el-input
+              ref="verifyCode"
+              v-model="loginForm.verifyCode"
+              placeholder="验证码"
+              name="verifyCode"
+              type="text"
+              tabindex="3"
+              autocomplete="on"
+              @keyup.enter.native="handleLogin"
+            />
+          </el-col>
+          <el-col :span="8" class="verifyCode">
+            <img id="verifyCodeImage" :src="image_verify_code" alt="" @click="getVerifyCode">
+          </el-col>
+        </el-row>
+      </el-form-item>
+
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
+
+      <!-- <div style="position:relative">
+        <div class="tips">
+          <span />
+          <span />
+        </div>
+        <div class="tips">
+          <span style="margin-right:18px;" />
+          <span />
+        </div>
+
+        <el-button class="thirdparty-button" type="primary" @click="showDialog=true">
+          第三方登陆
+        </el-button>
+      </div> -->
+    </el-form>
+
+    <!-- <el-dialog title="Or connect with" :visible.sync="showDialog">
+      Can not be simulated on local, so please combine you own business simulation! ! !
+      <br>
+      <br>
+      <br>
+      <social-sign />
+    </el-dialog> -->
+  </div>
+</template>
+
+<script>
+import { validUsername } from '@/utils/validate'
+import { v4 as uuidv4 } from 'uuid'
+// 导入验证码请求接口
+// import { getVerifyCode } from '@/api/user'
+// uuidv4()// ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+// import SocialSign from './components/SocialSignin'
+
+export default {
+  name: 'Login',
+  // components: { SocialSign },
+  data() {
+    const validateUsername = (rule, value, callback) => {
+      if (!validUsername(value)) {
+        callback(new Error('请输入正确的用户名或密码'))
+      } else {
+        callback()
+      }
+    }
+    const validatePassword = (rule, value, callback) => {
+      if (value.length < 4) {
+        callback(new Error('请输入正确的用户名或密码'))
+      } else {
+        callback()
+      }
+    }
+    const validateVerifyCode = (rule, value, callback) => {
+      if (value.length !== 4) {
+        callback(new Error('请输入正确的验证码'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      image_verify_code: '',
+      loginForm: {
+        username: 'yiliuyunting',
+        password: '123456',
+        verifyCode: '',
+        uuid: ''
+      },
+      loginRules: {
+        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
+        password: [{ required: true, trigger: 'blur', validator: validatePassword }],
+        verifyCode: [{ required: true, trigger: 'blur', validator: validateVerifyCode }]
+      },
+      passwordType: 'password',
+      capsTooltip: false,
+      loading: false,
+      // showDialog: false,
+      redirect: undefined,
+      otherQuery: {}
+    }
+  },
+  watch: {
+    $route: {
+      handler: function(route) {
+        const query = route.query
+        if (query) {
+          this.redirect = query.redirect
+          this.otherQuery = this.getOtherQuery(query)
+        }
+      },
+      immediate: true
+    }
+  },
+  created() {
+    // window.addEventListener('storage', this.afterQRScan)
+  },
+  mounted() {
+    if (this.loginForm.username === '') {
+      this.$refs.username.focus()
+    } else if (this.loginForm.password === '') {
+      this.$refs.password.focus()
+    }
+    this.getVerifyCode()
+  },
+  destroyed() {
+    // window.removeEventListener('storage', this.afterQRScan)
+  },
+  methods: {
+    // 获取二进制图片流并显示，该方法可以获取到图片流但无法正确显示
+    getVerifyCode() {
+      // alert('ok')
+      this.loginForm.uuid = uuidv4().split('-')[0] // ⇨ '9b1deb4d'
+      this.image_verify_code = process.env.VUE_APP_BASE_API + '/login/verificationCode?creationTime=' + this.loginForm.uuid
+      console.log(this.image_verify_code)
+      // 调用验证码请求方法
+      // getVerifyCode(this.uuid).then(response => {
+      //   console.log('response', response)
+      //   this.image_verify_code = 'data:image/jpeg;base64,' + encodeURI(response)
+      // })
+    },
+    checkCapslock(e) {
+      const { key } = e
+      this.capsTooltip = key && key.length === 1 && (key >= 'A' && key <= 'Z')
+    },
+    showPwd() {
+      if (this.passwordType === 'password') {
+        this.passwordType = ''
+      } else {
+        this.passwordType = 'password'
+      }
+      this.$nextTick(() => {
+        this.$refs.password.focus()
+      })
+    },
+    // 做登陆
+    handleLogin() {
+      // 做验证 验证用户名和密码是否合法   前端验证
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          this.loading = true
+          // 调用登陆的方法
+          this.$store.dispatch('user/login', this.loginForm)
+            .then(() => {
+              this.$router.push({ path: this.redirect || '/', query: this.otherQuery })
+              this.loading = false
+            })
+            .catch(() => {
+              this.loading = false
+              this.getVerifyCode()
+            })
+        } else {
+          this.getVerifyCode()
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    getOtherQuery(query) {
+      return Object.keys(query).reduce((acc, cur) => {
+        if (cur !== 'redirect') {
+          acc[cur] = query[cur]
+        }
+        return acc
+      }, {})
+    }
+    // afterQRScan() {
+    //   if (e.key === 'x-admin-oauth-code') {
+    //     const code = getQueryObject(e.newValue)
+    //     const codeMap = {
+    //       wechat: 'code',
+    //       tencent: 'code'
+    //     }
+    //     const type = codeMap[this.auth_type]
+    //     const codeName = code[type]
+    //     if (codeName) {
+    //       this.$store.dispatch('LoginByThirdparty', codeName).then(() => {
+    //         this.$router.push({ path: this.redirect || '/' })
+    //       })
+    //     } else {
+    //       alert('第三方登录失败')
+    //     }
+    //   }
+    // }
+  }
+}
+</script>
+
+<style lang="scss">
+/* 修复input 背景不协调 和光标变色 */
+/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
+
+$bg:#283443;
+$light_gray:#fff;
+$cursor: #fff;
+
+@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
+  .login-container .el-input input {
+    color: $cursor;
+  }
+}
+.verifyCode {
+  background-color: #fff!important;
+  padding: 8px 30px;
+}
+/* reset element-ui css */
+.login-container {
+  .el-input {
+    display: inline-block;
+    height: 47px;
+    width: 85%;
+
+    input {
+      background: transparent;
+      border: 0px;
+      -webkit-appearance: none;
+      border-radius: 0px;
+      padding: 12px 5px 12px 15px;
+      color: $light_gray;
+      height: 47px;
+      caret-color: $cursor;
+
+      &:-webkit-autofill {
+        box-shadow: 0 0 0px 1000px $bg inset !important;
+        -webkit-text-fill-color: $cursor !important;
+      }
+    }
+  }
+  .el-form {
+  .el-form-item {
+    &:nth-child(-n+3){
+      // border: none !important;
+      // background: transparent!important;
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    color: #454545;
+    }
+  }
+  // .el-form-item {
+  //   border: 1px solid rgba(255, 255, 255, 0.1);
+  //   background: rgba(0, 0, 0, 0.1);
+  //   border-radius: 5px;
+  //   color: #454545;
+  // }
+}
+}
+</style>
+
+<style lang="scss" scoped>
+$bg:#2d3a4b;
+$dark_gray:#889aa4;
+$light_gray:#eee;
+
+// .login-container {
+//   min-height: 100%;
+//   width: 100%;
+//   background-color: $bg;
+//   overflow: hidden;
+
+//   .login-form {
+//     position: relative;
+//     width: 520px;
+//     max-width: 100%;
+//     padding: 160px 35px 0;
+//     margin: 0 auto;
+//     overflow: hidden;
+//   }
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  padding-top: 160px;
+  background-color: $bg;
+  overflow: hidden;
+.logo {
+  margin: 20px auto;
+  text-align: center;
+  width: 50px;
+  img {
+    width: 100%;
+  }
+}
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 0 35px;
+    margin: 0 auto;
+    overflow: hidden;
+  }
+  .tips {
+    font-size: 14px;
+    color: #fff;
+    margin-bottom: 10px;
+
+    span {
+      &:first-of-type {
+        margin-right: 16px;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    width: 30px;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .thirdparty-button {
+    position: absolute;
+    right: 0;
+    bottom: 6px;
+  }
+
+  @media only screen and (max-width: 470px) {
+    .thirdparty-button {
+      display: none;
+    }
+  }
+    .el-col {
+    height: 47px;
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    color: #454545;
+  }
+}
+</style>
