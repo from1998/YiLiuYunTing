06af93036ccf0a9f54474be45f2ad756b841a932
田清深 <abel_shen1998@163.com>
2021-08-24 10:16:01 +0800

    日常push

--- src/views/system/user/componets/depotInfo.vue
+++ src/views/system/user/componets/depotInfo.vue
@@ -30,6 +30,7 @@
                 :props="{ expandTrigger: 'hover' }"
                 label-width="260px"
                 @change="handleChange"
+                @expand-change="expandChange"
               />
             </el-form-item>
           </el-col>
@@ -139,21 +140,7 @@
               </el-radio-group>
             </el-form-item>
           </el-col>
-        </el-row>
-        <!-- 所属角色与状态 -->
-        <el-row>
-          <el-col :span="12">
-            <el-form-item label="所属角色" prop="pertainRole">
-              <el-select v-model="form.pertainRole" placeholder="请选择所属角色">
-                <el-option
-                  v-for="item in pertainRoleOptions"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
-                />
-              </el-select>
-            </el-form-item>
-          </el-col>
+          <!-- 状态 -->
           <el-col :span="12">
             <el-form-item label="是否可用" prop="usable">
               <el-radio-group v-model="form.usable">
@@ -196,16 +183,15 @@
         </el-row>
         <el-form-item label="营业时间" prop="businessHours">
           <el-time-picker
-            v-model="form.businessHours"
+            v-model="businessHours"
             is-range
             range-separator="至"
             start-placeholder="开始时间"
             end-placeholder="结束时间"
             placeholder="选择时间范围"
+            value-format="hh-mm-ss"
+            @change="timeChange(businessHours)"
           />
-        </el-form-item>
-        <el-form-item label="备注" prop="remark">
-          <el-input v-model="form.remark" />
         </el-form-item>
         <el-row :gutter="30">
           <el-form-item>
@@ -222,10 +208,13 @@
 
 </template>
 <script>
+import area from '@/assets/json/citys.json'
+
 export default {
   name: 'DepotInfo',
   data() {
     return {
+      businessHours: [new Date(2016, 9, 10, 9, 0), new Date(2016, 9, 10, 17, 0)],
       form: {
         name: '',
         sName: '',
@@ -249,8 +238,9 @@ export default {
         uploadMonitorPlatform: '1',
         monitorPlatform: '',
         PlatformIdentification: '',
-        businessHours: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)],
-        remark: ''
+        remark: '',
+        start: '',
+        end: ''
       },
       categoryOptions: [{
         value: '选项1',
@@ -268,201 +258,7 @@ export default {
         value: '选项5',
         label: '北京烤鸭'
       }],
-      addressOptions: [{
-        value: 'zhinan',
-        label: '指南',
-        children: [{
-          value: 'shejiyuanze',
-          label: '设计原则',
-          children: [{
-            value: 'yizhi',
-            label: '一致'
-          }, {
-            value: 'fankui',
-            label: '反馈'
-          }, {
-            value: 'xiaolv',
-            label: '效率'
-          }, {
-            value: 'kekong',
-            label: '可控'
-          }]
-        }, {
-          value: 'daohang',
-          label: '导航',
-          children: [{
-            value: 'cexiangdaohang',
-            label: '侧向导航'
-          }, {
-            value: 'dingbudaohang',
-            label: '顶部导航'
-          }]
-        }]
-      }, {
-        value: 'zujian',
-        label: '组件',
-        children: [{
-          value: 'basic',
-          label: 'Basic',
-          children: [{
-            value: 'layout',
-            label: 'Layout 布局'
-          }, {
-            value: 'color',
-            label: 'Color 色彩'
-          }, {
-            value: 'typography',
-            label: 'Typography 字体'
-          }, {
-            value: 'icon',
-            label: 'Icon 图标'
-          }, {
-            value: 'button',
-            label: 'Button 按钮'
-          }]
-        }, {
-          value: 'form',
-          label: 'Form',
-          children: [{
-            value: 'radio',
-            label: 'Radio 单选框'
-          }, {
-            value: 'checkbox',
-            label: 'Checkbox 多选框'
-          }, {
-            value: 'input',
-            label: 'Input 输入框'
-          }, {
-            value: 'input-number',
-            label: 'InputNumber 计数器'
-          }, {
-            value: 'select',
-            label: 'Select 选择器'
-          }, {
-            value: 'cascader',
-            label: 'Cascader 级联选择器'
-          }, {
-            value: 'switch',
-            label: 'Switch 开关'
-          }, {
-            value: 'slider',
-            label: 'Slider 滑块'
-          }, {
-            value: 'time-picker',
-            label: 'TimePicker 时间选择器'
-          }, {
-            value: 'date-picker',
-            label: 'DatePicker 日期选择器'
-          }, {
-            value: 'datetime-picker',
-            label: 'DateTimePicker 日期时间选择器'
-          }, {
-            value: 'upload',
-            label: 'Upload 上传'
-          }, {
-            value: 'rate',
-            label: 'Rate 评分'
-          }, {
-            value: 'form',
-            label: 'Form 表单'
-          }]
-        }, {
-          value: 'data',
-          label: 'Data',
-          children: [{
-            value: 'table',
-            label: 'Table 表格'
-          }, {
-            value: 'tag',
-            label: 'Tag 标签'
-          }, {
-            value: 'progress',
-            label: 'Progress 进度条'
-          }, {
-            value: 'tree',
-            label: 'Tree 树形控件'
-          }, {
-            value: 'pagination',
-            label: 'Pagination 分页'
-          }, {
-            value: 'badge',
-            label: 'Badge 标记'
-          }]
-        }, {
-          value: 'notice',
-          label: 'Notice',
-          children: [{
-            value: 'alert',
-            label: 'Alert 警告'
-          }, {
-            value: 'loading',
-            label: 'Loading 加载'
-          }, {
-            value: 'message',
-            label: 'Message 消息提示'
-          }, {
-            value: 'message-box',
-            label: 'MessageBox 弹框'
-          }, {
-            value: 'notification',
-            label: 'Notification 通知'
-          }]
-        }, {
-          value: 'navigation',
-          label: 'Navigation',
-          children: [{
-            value: 'menu',
-            label: 'NavMenu 导航菜单'
-          }, {
-            value: 'tabs',
-            label: 'Tabs 标签页'
-          }, {
-            value: 'breadcrumb',
-            label: 'Breadcrumb 面包屑'
-          }, {
-            value: 'dropdown',
-            label: 'Dropdown 下拉菜单'
-          }, {
-            value: 'steps',
-            label: 'Steps 步骤条'
-          }]
-        }, {
-          value: 'others',
-          label: 'Others',
-          children: [{
-            value: 'dialog',
-            label: 'Dialog 对话框'
-          }, {
-            value: 'tooltip',
-            label: 'Tooltip 文字提示'
-          }, {
-            value: 'popover',
-            label: 'Popover 弹出框'
-          }, {
-            value: 'card',
-            label: 'Card 卡片'
-          }, {
-            value: 'carousel',
-            label: 'Carousel 走马灯'
-          }, {
-            value: 'collapse',
-            label: 'Collapse 折叠面板'
-          }]
-        }]
-      }, {
-        value: 'ziyuan',
-        label: '资源',
-        children: [{
-          value: 'axure',
-          label: 'Axure Components'
-        }, {
-          value: 'sketch',
-          label: 'Sketch Templates'
-        }, {
-          value: 'jiaohu',
-          label: '组件交互文档'
-        }]
-      }],
+      addressOptions: area,
       statusOptions: [
         {
           value: '1',
@@ -522,6 +318,16 @@ export default {
     // },
     handleChange(value) {
       console.log(value)
+    },
+    expandChange(value) {
+      // console.log(value)
+    },
+    timeChange(val) {
+      console.log(val)
+      this.form.start = val[0]
+      this.form.end = val[1]
+      console.log(this.form.start)
+      console.log(this.form.end)
     }
   }
 }
