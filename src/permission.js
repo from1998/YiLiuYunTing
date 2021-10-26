9adbf9b03af20cbd57c44456947e6be472712750
田清深 <abel_shen1998@163.com>
2021-10-26 11:10:12 +0800

    push

--- src/permission.js
+++ src/permission.js
@@ -8,7 +8,7 @@ import getPageTitle from '@/utils/get-page-title'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
-const whiteList = ['/login', '/auth-redirect', '/qrcodeAccess/accessIn', '/qrcodeAccess/accessOutNoPay', '/qrcodeAccess/accessOutPayed', '/qrcodeAccess/queryFails', '/qrcodeAccess/imprest'] // no redirect whitelist
+const whiteList = ['/login', '/auth-redirect', '/qrcodeAccess', '/qrcodeAccess/accessIn', '/qrcodeAccess/accessOutNoPay', '/qrcodeAccess/accessOutPayed', '/qrcodeAccess/queryFails', '/qrcodeAccess/imprest'] // no redirect whitelist
 
 router.beforeEach(async(to, from, next) => {
   // start progress bar
