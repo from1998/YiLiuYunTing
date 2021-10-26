f18f12eba7b8214563f01d868ec06e189495b36d
田清深 <abel_shen1998@163.com>
2021-10-26 11:30:07 +0800

    push

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div :class="classObj" class="app-wrapper">
     <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
-    <sidebar class="sidebar-container" />
+    <sidebar v-if="device!=='mobile'" class="sidebar-container" />
     <div :class="{hasTagsView:needTagsView}" class="main-container">
-      <div :class="{'fixed-header':fixedHeader}">
+      <div v-if="device!=='mobile'" :class="{'fixed-header':fixedHeader}">
         <navbar />
         <tags-view v-if="needTagsView" />
       </div>
       <app-main />
-      <right-panel v-if="showSettings">
+      <right-panel v-if="showSettings && device!=='mobile'">
         <settings />
       </right-panel>
     </div>
