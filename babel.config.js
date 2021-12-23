6c29b648ef6ff4819a7b82d7106c339d89724a1d
田清深 <abel_shen1998@163.com>
2021-12-23 11:03:20 +0800

    配置日志移除插件

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
+// 这是项目发布阶段需要用到的 bable 插件
+const prodPlugins = []
+// 发布阶段，则向`prodPlugins `数组中加入插件`transform-remove-console`
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
-  presets: [['@vue/app', { useBuiltIns: 'entry' }]]
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      'component',
+      {
+        libraryName: 'element-ui',
+        styleLibraryName: 'theme-chalk'
+      }
+    ],
+    // 发布产品时的插件数组
+    ...prodPlugins // 巧妙利用展开运算符
+  ]
 }
